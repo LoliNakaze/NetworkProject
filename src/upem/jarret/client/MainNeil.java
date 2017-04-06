1dc087a4a2905ceac8567e6eab147e3c148d7a11
ne-il <neil.anteur@yahoo.fr>
2017-04-07 00:12:44 +0200

    + de vérité

--- src/upem/jarret/client/MainNeil.java
+++ src/upem/jarret/client/MainNeil.java
@@ -4,5 +4,7 @@ public class MainNeil {
 	public static void main(String[] args) {
 		System.out.println("Guy est un suceur de bite");
 		System.out.println("Et en plus, c'est un gros bouffeur de CHATTES");
+		System.out.println("Et en plus, c'est un gros bouffeur de POILS");
+
 	}
 }
